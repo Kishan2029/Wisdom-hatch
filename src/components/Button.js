5ec193fa3c843def9ed985a0b2d898fc0ba98c61
kishan-2029 <kishan.prajapati@searce.com>
2023-07-05 15:10:33 +0530

    css changed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Button = ({ value, height = 14 }) => {
+    console.log("")
     return (
         <button className={`button text-sm font-semibold h-${height} w-fit px-5 rounded-xl `} >{value}</button>
     )
